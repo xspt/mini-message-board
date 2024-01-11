067a9d2353dda5964fab55af46bc220e92f2c001
xspt <82736724+xspt@users.noreply.github.com>
2024-01-11 07:15:02 -0300

    Add /new route and new message form view

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Message board', messages: messages });
 });
 
+router.get('/new', function(req, res, next) {
+   res.render('form');
+});
+
+router.post('/new', function(req, res, next) {
+   messages.push({ text: req.body.message, user: req.body.author, added: new Date() });
+   res.redirect('/');
+});
+
 module.exports = router;
