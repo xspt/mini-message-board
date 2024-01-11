0352d362c7cd584673e2a0535420f7d7ec52183e
xspt <82736724+xspt@users.noreply.github.com>
2024-01-11 06:55:39 -0300

    Add message board

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+const messages = [
+   {
+      text: 'Hi there!',
+      user: 'Amando',
+      added: new Date()
+   },
+   {
+      text: 'Hello world!',
+      user: 'Charles',
+      added: new Date()
+   }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Message board', messages: messages });
 });
 
 module.exports = router;
